500364a2202fd8470e17ab6c12cd45834b2dba21
Philippos Koumparos <filippos_7@hotmail.com>
2022-08-03 00:35:43 +0300

    Project 4 ready

--- Project 3/main.py
+++ Project 3/main.py
@@ -17,25 +17,25 @@ if algorithm == "FC-CBJ":
     #FC-CBJ uses backjumbing and the dom/wdeg heuristic
     result = backJumping_search(CSPobject,select_unassigned_variable=heuristic, inference=my_forward_checkingCBJ)
     print(result,'\nconstraintChecks = ',CSPobject.constraintChecks,'\nvisitedNodes =',CSPobject.visitedNodes)
-    print(time.time() - start,'\n')
+    print('Time taken: ',time.time() - start,'\n')
 
 elif algorithm == "FC":
     start = time.time()
     #FC uses backtracking and the dom/wdeg heuristic
     result = my_backtracking_search(CSPobject,select_unassigned_variable=heuristic, inference=my_forward_checking)
     print(result,'\nconstraintChecks = ',CSPobject.constraintChecks,'\nvisitedNodes =',CSPobject.visitedNodes)
-    print(time.time() - start,'\n')
+    print('Time taken: ',time.time() - start,'\n')
 
 elif algorithm == "MAC":
     start = time.time()
     #MAC uses backtracking and the dom/wdeg heuristic
     result = my_backtracking_search(CSPobject,select_unassigned_variable=heuristic, inference=my_mac)
     print(result,'\nconstraintChecks = ',CSPobject.constraintChecks,'\nvisitedNodes =',CSPobject.visitedNodes)
-    print(time.time() - start)
+    print('Time taken: ',time.time() - start)
 
 elif algorithm == "MIN-CONFLICTS":
     start = time.time()
     #min conflicts is simply min conflicts
     result = my_min_conflicts(CSPobject,1000)
     print(result,'\nconflicts =',CSPobject.conflictsCount)
-    print(time.time() - start)
+    print('Time taken: ',time.time() - start)
